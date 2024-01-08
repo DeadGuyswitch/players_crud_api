05ab2fadc7b80be6c3c76bf632397151cfc4ba54
DeadGuyswitch <nirdesh.ghost@gmail.com>
2024-01-08 11:04:06 +0545

    Added a ID field

--- pkg/players/models.go
+++ pkg/players/models.go
@@ -1,6 +1,7 @@
 package players
 
 type Player struct {
+	ID          int64  `json:"id"`
 	KitNumber   string `json:"kit_number"`
 	FirstName   string `json:"first_name"`
 	LastName    string `json:"last_name"`
