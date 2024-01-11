7b14c18fdd77d4654e51dd84b3368990b01737c1
DeadGuyswitch <nirdesh.ghost@gmail.com>
2024-01-11 08:42:13 +0545

    Implemented Get(), Update(), and Delete() methods for PostgreSQLPGXRepository

--- pkg/players/repository_postgresql_pgx.go
+++ pkg/players/repository_postgresql_pgx.go
@@ -2,6 +2,7 @@ package players
 
 import (
 	"context"
+	"database/sql"
 	"errors"
 	"github.com/jackc/pgconn"
 	"github.com/jackc/pgx/v4/pgxpool"
@@ -51,12 +52,66 @@ func (r *PostgreSQLPGXRepository) Create(ctx context.Context, player Player) (*P
 	return &player, nil
 }
 
-//func (r *PostgreSQLPGXRepository) List(ctx context.Context) ([]Player, error) {
-//	rows, err := r.db.Query("SELECT * from players")
-//	if err != nil {
-//		return nil, err
-//	}
-//	defer rows.Close()
-//
-//	var playersList []Player
-//}
+func (r *PostgreSQLPGXRepository) List(ctx context.Context) ([]Player, error) {
+	rows, err := r.db.Query(ctx, "SELECT * from players")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var playersList []Player
+	for rows.Next() {
+		var player Player
+		if err := rows.Scan(&player.ID, &player.FirstName, &player.LastName, &player.Nationality, &player.Position); err != nil {
+			return nil, err
+		}
+
+		playersList = append(playersList, player)
+	}
+	return playersList, nil
+}
+
+func (r *PostgreSQLPGXRepository) Get(ctx context.Context, firstName string, lastName string) (*Player, error) {
+	row := r.db.QueryRow(ctx, "SELECT * FROM players WHERE firstName = $1 AND lastName = $2", firstName, lastName)
+
+	var player Player
+	if err := row.Scan(&player.ID, &player.FirstName, &player.LastName, &player.Nationality, &player.Position); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNotExist
+		}
+		return nil, err
+	}
+	return &player, nil
+}
+
+func (r *PostgreSQLPGXRepository) Update(ctx context.Context, id int64, updated Player) (*Player, error) {
+	res, err := r.db.Exec(ctx, "UPDATE players SET kitNumber = $1, firstName = $2, lastName = $3, nationality = $4, pos = $5",
+		updated.KitNumber, updated.FirstName, updated.LastName, updated.Nationality, updated.Position)
+	if err != nil {
+		var pgxError *pgconn.PgError
+		if errors.As(err, &pgxError) {
+			if pgxError.Code == "23505" {
+				return nil, ErrDuplicate
+			}
+		}
+		return nil, err
+	}
+
+	rowsAffected := res.RowsAffected()
+	if rowsAffected == 0 {
+		return nil, ErrUpdateFailed
+	}
+	return &updated, nil
+}
+
+func (r *PostgreSQLPGXRepository) Delete(ctx context.Context, id int64) error {
+	res, err := r.db.Exec(ctx, "DELETE FROM players WHERE id = $1", id)
+	if err != nil {
+		return err
+	}
+	rowsAffected := res.RowsAffected()
+	if rowsAffected == 0 {
+		return ErrDeleteFailed
+	}
+	return err
+}
